8c038a0678bcc0a0be0ae624518f1373c2f0ab6d
Saif <ahmads87237@gmail.com>
2026-01-19 10:43:41 +0545

    updated

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ import addressRouter from "./routes/addressRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 import path from "path";
 import { fileURLToPath } from "url";
+import history from "connect-history-api-fallback";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -19,6 +20,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Connect to DB & Cloudinary
 await ConnectDb();
 await connectCloudinary();
 
@@ -35,12 +37,14 @@ app.use("/api/cart", cartRouter);
 app.use("/api/address", addressRouter);
 app.use("/api/order", orderRouter);
 
-// Serve frontend build
+// Serve frontend build with SPA fallback
+app.use(history({
+  index: '/index.html',
+  rewrites: [
+    { from: /^\/api\/.*$/, to: (context) => context.parsedUrl.pathname } // ignore API routes
+  ]
+}));
 app.use(express.static(path.join(__dirname, "../client/dist")));
-
-app.get( (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-});
 
 // Start server
 app.listen(port, () => {
