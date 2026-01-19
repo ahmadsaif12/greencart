32ff68ddcce26e8d68477b6f93372dd5ad29bde9
Saif <ahmads87237@gmail.com>
2026-01-19 09:37:05 +0545

    updated

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
-const ConnectDb=async()=>{
-    try{
-        mongoose.connection.on('connected',()=>console.log("connected to database"));
-        await mongoose.connect(
+const ConnectDb = async () => {
+  try {
+    mongoose.connection.on("connected", () =>
+      console.log("connected to database")
+    );
+    await mongoose.connect(
       `${process.env.MONGODB_URI}/greencart`
     );
-    }catch(error){
-        console.error(error.message)
-    }
-}
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
 export default ConnectDb;
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -7,6 +7,6 @@ const userRoute = express.Router();
 userRoute.post("/register", register);
 userRoute.post("/login",login);
 userRoute.get("/is-auth",authUser,isAuth);
-userRoute.get("/logout",authUser,logout)
+userRoute.get("/logout",authUser,logout);
 
 export default userRoute;
