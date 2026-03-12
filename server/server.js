cdb606a4b5e9a601df3adabd0770cea89f205031
ahmadsaif12 <ahmads87237@gmail.com>
2026-03-12 20:55:42 +0545

    changes

--- server/server.js
+++ server/server.js
@@ -27,15 +27,12 @@ const port = process.env.PORT || 5000;
 await ConnectDb();
 await connectCloudinary();
 
-// 2. Middleware (CORS MUST come before Routes)
-// app.use(cors({ 
-//   origin: ["http://localhost:5173", "http://localhost:5174"], 
-//   credentials: true,
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   allowedHeaders: ["Content-Type", "Authorization"]
-// }));
-app.use(cors());
-
+app.use(cors({ 
+  origin: ["http://localhost:5173", "http://localhost:5174"], 
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use(cookieParser());
 
